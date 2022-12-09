5fa1f6a2355557b7c4fb8ba6be93d7375bbb9062
Gary Hemming <garyhemming@me.com>
2022-12-09 08:46:28 +0000

    Column chooser for dataset viewer

--- R/mod_datasetViewer.R
+++ R/mod_datasetViewer.R
@@ -10,14 +10,38 @@
 mod_datasetViewer_ui <- function(id){
   ns <- NS(id)
   tagList(
-    fluidRow(column(width = 12,
-                    align = 'right',
-                    checkboxInput(inputId=ns('transpose'), label='Transpose', value = FALSE))
-             ),
-    fluidRow(column(width = 12,
-                    DT::DTOutput(ns('data'))
-                    )
-             )
+    fluidRow(
+      column(
+        width = 6
+      ),
+      column(
+        width = 3,
+        align = 'right',
+        div(
+          style = 'margin-top:20px',
+          checkboxInput(inputId=ns('transpose'), label='Transpose', value = FALSE)
+        )
+      ),
+      column(
+        width = 3,
+        align = 'right',
+        div(
+          style = 'margin-top:15px',
+          radioGroupButtons(
+            inputId = ns('columns_to_display'),
+            label = NULL,
+            choices = c('Dataset','Lucidum','All'),
+            selected = 'Dataset'
+            )
+          )
+        )
+      ),
+    fluidRow(
+      column(
+        width = 12,
+        DT::DTOutput(ns('data'))
+        )
+      )
   )
 }
     
@@ -29,19 +53,32 @@ mod_datasetViewer_server <- function(id, d, dt_update){
     ns <- session$ns
     output$data <- DT::renderDT({
       dt_update()
-      format_dataset_for_DT(d(), input$transpose)
+      format_dataset_for_DT(d(), input$transpose, input$columns_to_display)
     })
   })
 }
 
-format_dataset_for_DT <- function(d, transpose){
+format_dataset_for_DT <- function(d, transpose, columns_to_display){
   max_rows_to_display <- 100
   max_cols <- 100
+  # apply filter
   if('total_filter' %in% names(d)){
     d_filter <- d[which(total_filter==1)]
   } else {
     d_filter <- d
   }
+  # only include selected cols
+  all_cols <- names(d_filter)
+  non_lucidum_cols <- remove_lucidum_cols(all_cols)
+  lucidum_cols <- setdiff(all_cols, non_lucidum_cols)
+  if(columns_to_display=='Dataset'){
+    d_filter <- d_filter[, ..non_lucidum_cols]
+  } else if(columns_to_display=='Lucidum'){
+    d_filter <- d_filter[, ..lucidum_cols]
+  } else if(columns_to_display=='All'){
+    
+  }
+  # transpose if selected
   if(transpose==FALSE){
     pg_length <- min(max_rows_to_display, nrow(d_filter))
   } else {
@@ -55,6 +92,7 @@ format_dataset_for_DT <- function(d, transpose){
     names(d_filter) <- c('dataset_column', as.character(idx))
     pg_length <- min(1000, nrow(d_filter))
   }
+  # render DT
   dt <- DT::datatable(d_filter,
                       rownames= FALSE,
                       extensions = 'Buttons',
