bddae4b826851efc972227b3d553008a12d23a79
Gary Hemming <garyhemming@me.com>
2022-12-13 09:07:37 +0000

    Delete glm_prediction col before recreating

--- R/mod_GlimmaR.R
+++ R/mod_GlimmaR.R
@@ -39,6 +39,7 @@ mod_GlimmaR_server <- function(id, d, dt_update, response, weight, feature_spec,
         # copy model predictions to d
         rows_idx <- GlimmaR_models()[[GlimmaR_idx()]]$pred_rows
         preds <- GlimmaR_models()[[GlimmaR_idx()]]$predictions
+        d()[, glm_prediction:= NULL]
         d()[rows_idx, glm_prediction := preds]
         dt_update(dt_update()+1)
         # copy LP cols
