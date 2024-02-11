aa0647b67fb3dfd83ca5d8ac57d1c5a8e49235c6
Gary Hemming <garyhemming@me.com>
2024-02-11 09:07:17 +0000

    Update description file

--- app.R
+++ app.R
@@ -1,9 +1,6 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
-
-# QUESTION - what is the next line doing exactly?
-# when my app is "finished" could I just use library(lucidum)?
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options('golem.app.prod' = TRUE)
 
